b447015517c7e1d7df310e2966e4b45c1d8b1ddc
HackProtector <dhirajworkspace777@gmail.com>
2026-07-16 21:32:33 +0530

    Update SC.js

--- SC.js
+++ SC.js
@@ -1,28 +1,44 @@
-// (function () {
+(function() {
+    var devtools = /./;
+    devtools.toString = function() {
+        this.opened = true;
+    };
+    
+    setInterval(function() {
+        var before = new Date().getTime();
+        debugger;
+        var after = new Date().getTime();
+        if (after - before > 100) {
+            document.body.innerHTML = "Access Denied!";
+        }
+    }, 1000);
+})();
 
-//     document.querySelectorAll('link[rel="stylesheet"]').forEach(e => e.remove());
+(function () {
 
-//     document.querySelectorAll("style").forEach(e => e.remove());
-//     document.querySelectorAll("*").forEach(el => {
-//         el.removeAttribute("style");
-//         el.removeAttribute("class");
-//         el.removeAttribute("id");
-//     });
-//     new MutationObserver(mutations => {
-//         mutations.forEach(m => {
-//             m.addedNodes.forEach(node => {
-//                 if (
-//                     node.tagName === "STYLE" ||
-//                     (node.tagName === "LINK" && node.rel === "stylesheet")
-//                 ) {
-//                     node.remove();
-//                 }
-//             });
-//         });
-//     }).observe(document.documentElement, {
-//         childList: true,
-//         subtree: true
-//     });
+    document.querySelectorAll('link[rel="stylesheet"]').forEach(e => e.remove());
 
-// })();
+    document.querySelectorAll("style").forEach(e => e.remove());
+    document.querySelectorAll("*").forEach(el => {
+        el.removeAttribute("style");
+        el.removeAttribute("class");
+        el.removeAttribute("id");
+    });
+    new MutationObserver(mutations => {
+        mutations.forEach(m => {
+            m.addedNodes.forEach(node => {
+                if (
+                    node.tagName === "STYLE" ||
+                    (node.tagName === "LINK" && node.rel === "stylesheet")
+                ) {
+                    node.remove();
+                }
+            });
+        });
+    }).observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+
+})();
  // console.log("Clear");
